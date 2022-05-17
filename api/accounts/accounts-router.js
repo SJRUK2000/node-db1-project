847353a054e8c1d65fb08da68ba993141c96d7c9
Sean-Ubele <seanjohnreedubeleknuckles@gmail.com>
2022-05-17 01:52:28 -0400

    22 passed 2 failed

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,31 +1,38 @@
 const router = require('express').Router()
 const md = require('./accounts-middleware')
+const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-      res.json('get accounts')
+    const accounts = await Account.getAll()
+    res.json(accounts)
   } catch (err) {
     next (err)
   }
 })
 
-router.get('/:id', md.checkAccountId, (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-    res.json('get accounts by id')
-  } catch (err) {
-    next(err)
-  }
+router.get('/:id', md.checkAccountId, async (req, res, next) => {
+ try {
+   const account = await Account.getById(req.params.id)
+   res.json(account)
+ } catch(err){
+   next(err)
+ }
 })
 
-router.post('/',
+router.post(
+  '/',
   md.checkAccountPayload,
   md.checkAccountNameUnique,
-  (req, res, next) => {
+  async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    res.json('post account')
+    const newAccount = await Account.create({
+      name : req.body.name.trim(),
+      budget : req.body.budget
+    })
+    res.status(201).json(newAccount)
   } catch (err) {
     next(err)
   }
@@ -35,8 +42,10 @@ router.put('/:id',
   md.checkAccountId,
   md.checkAccountPayload,
   md.checkAccountNameUnique,
-  (req, res, next) => {
+  async (req, res, next) => {
     // DO YOUR MAGIC
+    const updated = await Account.updateById(req.params.id, req.body)
+    res.json(updated)
     try { 
       res.json('update account')
     } catch (err) {
@@ -46,10 +55,10 @@ router.put('/:id',
 
 router.delete('/:id',
 md.checkAccountId, 
- (req, res, next) => {
-  // DO YOUR MAGIC
+ async (req, res, next) => {
   try {
-    res.json('delete account')
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
   } catch (err) {
     next(err)
   }
